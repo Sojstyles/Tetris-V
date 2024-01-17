c6681938be815c5cc02042da463291b767518df3
Olasoji <65076620+Sojstyles@users.noreply.github.com>
2024-01-17 12:40:59 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mytechstellar3" # Replace with your actual S3 bucket name
+    bucket = "mytechstellars3" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
